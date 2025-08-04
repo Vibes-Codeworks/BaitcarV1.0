d8b794f8efc17c8556b0f9535fde9894f428dfd2
EvanM222 <Bayleemusick.22@gmail.com>
2025-08-04 18:02:38 -0500

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 Config = {}
 
 Config.Police = {
-    'license:7abf6d9a69a136aadf2c95b619a96839f053eb79' --license
+    'license:' --license
 }
 
 Config.AlarmTime = 30000 --milliseconds default 30 seconds
